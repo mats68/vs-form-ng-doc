4d3c1898774d814c1c381ca35170c124f2612eea
M. Thaler <mats68@gmail.com>
2020-10-04 23:11:39 +0200

    remove scully

--- scully.vs-form-ng-doc.config.ts
+++
@@ -1,8 +0,0 @@
-import { ScullyConfig } from '@scullyio/scully';
-export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "vs-form-ng-doc",
-  outDir: './dist/static',
-  routes: {
-  }
-};
